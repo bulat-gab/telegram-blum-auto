f1a0273e8fb3a8d570e40620996543cf71656fe9
Bulat Gabdrakhmanov <gabdrakhmanov.br@gmail.com>
2024-06-18 22:58:56 +0700

    Add isntall-service.sh

---
+++ install-service.sh
@@ -0,0 +1,3 @@
+sudo cp ./blumbot.service /etc/systemd/system/blumbot.service
+sudo systemctl daemon-reload
+sudo systemctl enable blumbot.service
--- kill-blum-bot.sh
+++
@@ -1,8 +0,0 @@
-# Проверка на наличие папки venv
-echo "Killing process run-blum-bot.sh ..."
-pkill -f run-blum-bot.sh
-
-echo "Deactivatting Python virtual environment"
-deactivate
-
-echo "done"
